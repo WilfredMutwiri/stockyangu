9cf60eb15e730931f62fa9fafd786ac61778e608
Gerison Kimathi <gerkim62@gmail.com>
2024-10-22 21:45:15 +0300

    update SameSite attribute in cookie settings to 'none' for cross-site requests

--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -126,7 +126,7 @@ userRouter.post(
     res.cookie("token", token, {
       httpOnly: true, // Prevent JavaScript access to the cookie
       secure: true, // Use Secure flag (only send over HTTPS)
-      sameSite: "strict", // Prevent CSRF attacks
+      sameSite: "none", // Prevent CSRF attacks
       maxAge: 24 * 60 * 60 * 1000, // time in milliseconds
     });
 
