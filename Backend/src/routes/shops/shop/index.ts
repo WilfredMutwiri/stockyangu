da4cd22ac05f93fa60cf5cc97ef98872cb4a01ce
Gerison Kimathi <gerkim62@gmail.com>
2024-10-26 21:30:06 +0300

    implement shop retrieval logic with role-based access control; handle errors and responses for different user roles

--- Backend/src/routes/shops/shop/index.ts
+++ Backend/src/routes/shops/shop/index.ts
@@ -1,16 +1,97 @@
-import { Router } from "express";
+import { Shop, UserRole } from "@prisma/client";
+import { Response, Router } from "express";
+import { ApiResponseType } from "../../../types/api";
 import productsRouter from "./products";
+import prisma from "../../../lib/prisma";
 
 const shopRouter = Router();
 
 shopRouter.use("/products", productsRouter);
 
+shopRouter.get("/", async (req, res: Response<ApiResponseType<Shop, null>>) => {
+  try {
+    const role = req.user.role;
 
-shopRouter.get("/", (_, res) => {
-  // if the logged in user is a shop owner (or can view), we can return the shop details
-  // already the id is in the req.params.id or "my" for the logged in user
-  res.send("Shop route get hit...");
+    if (role === UserRole.SELLER) {
+      return res.status(403).json({
+        success: false,
+        message: "You do not have permission to view the requested data.",
+        data: null,
+      });
+    }
+
+    if (role === UserRole.MANAGER) {
+      const userWithShop = await prisma.user.findUnique({
+        where: {
+          id: req.user.id,
+        },
+        select: {
+          shop: true,
+        },
+      });
+
+      if (
+        !userWithShop ||
+        !userWithShop.shop ||
+        userWithShop.shop.id !== req.user.shopId
+      ) {
+        return res.status(404).json({
+          success: false,
+          message: "The requested data was not found.",
+          data: null,
+        });
+      }
+
+      return res.json({
+        success: true,
+        message: "Succeeded.",
+        data: userWithShop.shop,
+      });
+    }
+
+    if (role === UserRole.SYS_ADMIN) {
+      const shopId = req.query["id"];
+      if (!shopId || typeof shopId !== "number") {
+        return res.status(400).json({
+          success: false,
+          message: "Please provide a valid shop id.",
+          data: null,
+        });
+      }
+      const shop = await prisma.shop.findUnique({
+        where: {
+          id: shopId,
+        },
+      });
+
+      if (!shop) {
+        return res.status(404).json({
+          success: false,
+          message: "The requested data was not found.",
+          data: null,
+        });
+      }
+
+      return res.json({
+        success: true,
+        message: "Succeeded.",
+        data: shop,
+      });
+    }
+
+    return res.status(400).json({
+      success: false,
+      message: "Invalid request. (code: N.R)",
+      data: null,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+      data: null,
+    });
+  }
 });
-
 
 export default shopRouter;
