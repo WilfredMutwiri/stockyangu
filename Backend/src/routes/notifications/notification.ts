06932c59014dc6ead34edfb629b33297a26edf0f
Gerison Kimathi <gerkim62@gmail.com>
2024-11-01 08:57:10 +0300

    enhance notifications route; add endpoint to mark a notification as read

--- Backend/src/routes/notifications/notification.ts
+++ Backend/src/routes/notifications/notification.ts
@@ -1,7 +1,7 @@
 import { Notification } from "@prisma/client";
 import { Request, Response, Router } from "express";
 import { ApiResponseType } from "../../types/api";
-import prisma from "../../lib/prisma";
+import prisma, { nullOnNotFound } from "../../lib/prisma";
 
 const notificationRouter = Router({
   mergeParams: true,
@@ -50,6 +50,53 @@ notificationRouter.get(
   }
 );
 
+// mark as read
+notificationRouter.patch(
+  "/mark-as-read",
+  async (
+    req: Request<{
+      notificationId: string;
+    }>,
+    res: Response<ApiResponseType<Notification, null>>
+  ) => {
+    try {
+      const notificationId = Number(req.params.notificationId);
 
+      // mark as read
+      const notification = await nullOnNotFound(
+        prisma.notification.update({
+          where: {
+            id: notificationId,
+            userId: req.user.id,
+          },
+          data: {
+            read: true,
+          },
+        })
+      );
+
+      if (!notification) {
+        return res.status(404).json({
+          success: false,
+          message: "Notification not found.",
+          data: null,
+        });
+      }
+
+      return res.json({
+        success: true,
+        message: "Succeeded.",
+        data: notification,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error. Please retry.",
+        data: null,
+      });
+    }
+  }
+);
 
 export default notificationRouter;
