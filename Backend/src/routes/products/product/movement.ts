364bbf3030bf0bc1a32dc62cfd4baf982e285f9b
Gerison Kimathi <gerkim62@gmail.com>
2024-11-02 10:44:16 +0300

    add POST route for creating product movements with validation and price association

--- Backend/src/routes/products/product/movement.ts
+++ Backend/src/routes/products/product/movement.ts
@@ -1,7 +1,8 @@
 import { Request, Response, Router } from "express";
 import { ApiResponseType } from "../../../types/api";
-import { ProductMovement } from "@prisma/client";
+import { Price, ProductMovement } from "@prisma/client";
 import prisma from "../../../lib/prisma";
+import { ProductMovementSchema } from "../../../validation/productMovement";
 
 const productMovementRouter = Router({ mergeParams: true });
 
@@ -31,6 +32,15 @@ productMovementRouter.get(
           id: productId,
           shopId: req.user.shopId,
         },
+        include: {
+          movements: {
+            take: limit,
+            skip: offset,
+            orderBy: {
+              createdAt: "desc",
+            },
+          },
+        },
       });
 
       if (!product) {
@@ -42,16 +52,7 @@ productMovementRouter.get(
       }
 
       // get product movements
-      const productMovements = await prisma.productMovement.findMany({
-        where: {
-          productId,
-        },
-        take: limit,
-        skip: offset,
-        orderBy: {
-          createdAt: "desc",
-        },
-      });
+      const productMovements = product.movements;
 
       const count = await prisma.productMovement.count({
         where: {
@@ -83,6 +84,102 @@ productMovementRouter.get(
                 : null,
             self: req.originalUrl,
           },
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error. Please retry.",
+        data: [],
+      });
+    }
+  }
+);
+
+productMovementRouter.post(
+  "/",
+  async (
+    req: Request<{
+      productId: string;
+    }>,
+    res: Response<
+      ApiResponseType<
+        ProductMovement & {
+          price: Price;
+        }
+      >
+    >
+  ) => {
+    try {
+      const productId = Number(req.params.productId);
+      if (!req.user.shopId) {
+        return res.status(404).json({
+          success: false,
+          message: `You are not a worker in any shop.`,
+          data: [],
+        });
+      }
+
+      const validationResult = ProductMovementSchema.safeParse(req.body);
+
+      if (!validationResult.success) {
+        return res.status(400).json({
+          success: false,
+          message: "Validation failed.",
+          data: validationResult.error.issues,
+        });
+      }
+
+      //   check if the product exists
+      const product = await prisma.product.findUnique({
+        where: {
+          id: productId,
+          shopId: req.user.shopId,
+        },
+        include: {
+          priceHistory: {
+            where: {
+              endDate: null,
+            },
+          },
+        },
+      });
+
+      if (!product) {
+        return res.status(404).json({
+          success: false,
+          message: "Product not found.",
+          data: [],
+        });
+      }
+
+      const priceInEffect = product.priceHistory[0];
+
+      if (!priceInEffect) {
+        return res.status(400).json({
+          success: false,
+          message: "Please set an active price for this product first.",
+          data: [],
+        });
+      }
+
+      // create product movement
+      const productMovement = await prisma.productMovement.create({
+        data: {
+          ...validationResult.data,
+          productId,
+          productPriceId: priceInEffect.id,
+          workerId: req.user.id,
+        },
+      });
+
+      return res.json({
+        success: true,
+        message: "Succeeded.",
+        data: {
+          ...productMovement,
+          price: priceInEffect,
         },
       });
     } catch (error) {
